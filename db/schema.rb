5c89949c14a05fe489877b23788832c5f9ca444a
Eugene Millerman <e.v.millerman@gmail.com>
2014-02-01 15:15:37 -0500

    Make a basic User model (including secure passwords)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140201000313) do
+ActiveRecord::Schema.define(:version => 20140201011935) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
